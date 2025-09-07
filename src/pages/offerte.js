21f3eaf6730c7ab12f447813157f9a669ca07498
Muhammet <muhammet075@icloud.com>
2025-09-07 22:39:35 +0200

    working on offerte system

--- src/pages/offerte.js
+++ src/pages/offerte.js
@@ -4,6 +4,8 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "@/styles/offerte.module.css";
 import euFlag from "@/assets/icons/eu-flag.svg";
+import keus1 from "@/assets/img/keus1.png";
+import keus2 from "@/assets/img/keus2.png";  
 
 export default function Offerte() {
   useEffect(() => {
@@ -16,10 +18,12 @@ export default function Offerte() {
   let kentekenCorrect = false
   let invoerKenteken;
   let voertuigGegevens;
+  let gekozenWens;
 
     function checkKenteken(e){
         const value = e.target.value.replace(/[^a-zA-Z0-9]/g, "");
         e.target.value = value;
+        document.querySelector("#kentekenmerk").innerHTML 
 
         document.querySelector(".kentekeninput").classList.remove("successtate");
         document.querySelector(".kentekeninput").classList.remove("errorstate");
@@ -36,6 +40,7 @@ export default function Offerte() {
 
         } else if(e.target.value.length < 6){
             kentekenIngevuld = false;
+            kentekenCorrect = false
             console.log(kentekenIngevuld);
         };
     };
@@ -64,6 +69,7 @@ async function zoekKenteken() {
     setTimeout(() => {
         document.querySelector(".kentekenloadingstate").classList.add("displaynone"); 
         document.querySelector(".kentekeninput").classList.add("successtate");
+        document.querySelector("#kentekenmerk").innerHTML = jsondata[0].merk + " " + jsondata[0].handelsbenaming + " (" + jsondata[0].datum_eerste_toelating.substring(0, 4) + ")";
         kentekenCorrect = true;
 
         voertuigGegevens = {
@@ -84,6 +90,19 @@ async function zoekKenteken() {
 
 
 
+  function checkKeuze(e){
+        document.querySelector(".stap2errorstate").innerHTML = "";
+
+       if (e.target.id === "stap2-keus1"){
+        document.querySelector("#stap2-keus1").classList.add("selected");
+        document.querySelector("#stap2-keus2").classList.remove("selected");
+        gekozenWens = "CarPlay op het originele scherm";
+       } else if (e.target.id === "stap2-keus2"){
+        document.querySelector("#stap2-keus2").classList.add("selected");
+        document.querySelector("#stap2-keus1").classList.remove("selected");
+        gekozenWens = "Nieuwe groter scherm met CarPlay";
+       };
+ };
 
 
 
@@ -107,7 +126,12 @@ async function zoekKenteken() {
 
 
 
-    function volgendeStap2(){
+    function volgendeStap2(){        
+        if(!gekozenWens){
+            document.querySelector(".stap2errorstate").innerHTML = "Maak een keuze om verder te gaan.";
+            return;
+        };
+
         document.querySelector(".stap2").classList.add("displaynone");
         document.querySelector(".stap3").classList.remove("displaynone");
         document.querySelector("#stap2-sign").classList.remove("activestap");
@@ -167,6 +191,7 @@ function vorigeStap2(){
                     <div className={`${styles.kenteken} kentekeninput`}>
                         <div><Image src={euFlag} alt="Europese unie vlag"/><p>NL</p></div>
                         <input type="text" id="kentekeninputfield" onChange={checkKenteken} maxLength={6}></input>
+                        <p id="kentekenmerk"></p>
                     </div>
 
                     <div className={`${styles.kentekenloadingstate} kentekenloadingstate displaynone`}>
@@ -180,7 +205,14 @@ function vorigeStap2(){
 
                 <section className="stap2 displaynone">
                     <h2>2. Wat zijn uw wensen?</h2>
-                    <p>Hier komen de wensen</p>
+                    <p>Wilt u Apple CarPlay & Android Auto op uw huidige originele scherm of wilt u een groter scherm met Apple CarPlay & Android Auto?</p>
+
+                    <ul>
+                        <li onClick={checkKeuze} className="stap2keus" id="stap2-keus1"><Image src={keus1}/><p>CarPlay op het originele scherm</p></li>
+                        <li onClick={checkKeuze} className="stap2keus" id="stap2-keus2"><Image src={keus2}/><p>Nieuwe groter scherm met CarPlay</p></li>
+                    </ul>
+
+                    <p className="stap2errorstate"></p>
 
 
                     <button onClick={volgendeStap2} className={styles.volgendestap}>Volgende stap</button>
