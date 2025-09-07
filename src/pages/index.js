231da19ddd906df29c2640da9dde5adb82f6ab33
Muhammet <muhammet075@icloud.com>
2025-09-07 21:43:22 +0200

    working on offerte system

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   useEffect(() => {
@@ -8,13 +9,19 @@ export default function Home() {
   return (
     <>
       <Head>
-          <title>Next</title>
+          <title>AutoDraadloos.nl</title>
           <meta name="description" content=""/>
           <meta name="og:description" content=""/>
           <meta name="keywords" content=""/>
           <meta property="og:url" content=""/>
           <meta property="og:title" content=""/>
       </Head>
+
+      <div>
+        <h1>Website in aanbouw</h1>
+        <p>Klik hieronder om een offerte voor uw voertuig te krijgen.</p>
+        <Link href="/offerte">Offerte</Link>
+      </div>
     </>
   );
 }
---
+++ src/pages/offerte.js
@@ -0,0 +1,212 @@
+import { useEffect } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "@/styles/offerte.module.css";
+import euFlag from "@/assets/icons/eu-flag.svg";
+
+export default function Offerte() {
+  useEffect(() => {
+  });
+
+
+
+
+  let kentekenIngevuld = false
+  let kentekenCorrect = false
+  let invoerKenteken;
+  let voertuigGegevens;
+
+    function checkKenteken(e){
+        const value = e.target.value.replace(/[^a-zA-Z0-9]/g, "");
+        e.target.value = value;
+
+        document.querySelector(".kentekeninput").classList.remove("successtate");
+        document.querySelector(".kentekeninput").classList.remove("errorstate");
+
+        if(e.target.value.length === 6){
+            kentekenIngevuld = true;
+            console.log(kentekenIngevuld);
+             
+            invoerKenteken = e.target.value
+
+            document.querySelector(".kentekenloadingstate").classList.remove("displaynone");
+
+            zoekKenteken();
+
+        } else if(e.target.value.length < 6){
+            kentekenIngevuld = false;
+            console.log(kentekenIngevuld);
+        };
+    };
+
+
+
+
+
+async function zoekKenteken() {
+    let kentekenUpperCase = invoerKenteken.toUpperCase();
+
+      const kenteken =
+        "https://opendata.rdw.nl/resource/m9d7-ebf2.json?kenteken=" + kentekenUpperCase;
+
+      const response = await fetch(kenteken);
+      const jsondata = await response.json();
+
+      if (jsondata.length === 0) {
+        setTimeout(() => {
+            document.querySelector(".kentekenloadingstate").classList.add("displaynone");
+            document.querySelector(".kentekeninput").classList.add("errorstate");
+        }, 2000);
+        return;
+    };
+
+    setTimeout(() => {
+        document.querySelector(".kentekenloadingstate").classList.add("displaynone"); 
+        document.querySelector(".kentekeninput").classList.add("successtate");
+        kentekenCorrect = true;
+
+        voertuigGegevens = {
+            merk: jsondata[0].merk,
+            handelsbenaming: jsondata[0].handelsbenaming,
+            kenteken: jsondata[0].kenteken,
+            bouwjaar: jsondata[0].datum_eerste_toelating.substring(0, 4),
+            carrosserie: jsondata[0].inrichting,
+        };
+
+        console.log(voertuigGegevens); 
+
+
+
+    }, 2000);
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+  function volgendeStap1(){
+    let kentekeninputfield = document.querySelector("#kentekeninputfield").value;
+    if(kentekenCorrect === false || kentekeninputfield.length < 6){
+        document.querySelector(".kentekeninput").classList.remove("successtate");
+        document.querySelector(".kentekeninput").classList.add("errorstate");
+        return;
+    };
+    document.querySelector(".stap1").classList.add("displaynone");
+    document.querySelector(".stap2").classList.remove("displaynone");
+    document.querySelector("#stap1-sign").classList.remove("activestap");
+    document.querySelector("#stap2-sign").classList.add("activestap");
+  };
+
+
+
+    function volgendeStap2(){
+        document.querySelector(".stap2").classList.add("displaynone");
+        document.querySelector(".stap3").classList.remove("displaynone");
+        document.querySelector("#stap2-sign").classList.remove("activestap");
+        document.querySelector("#stap3-sign").classList.add("activestap");
+    }
+
+  function vorigeStap1(){
+    document.querySelector(".stap2").classList.add("displaynone");
+    document.querySelector(".stap1").classList.remove("displaynone");
+    document.querySelector("#stap2-sign").classList.remove("activestap");
+    document.querySelector("#stap1-sign").classList.add("activestap");
+};
+
+function vorigeStap2(){
+    document.querySelector(".stap3").classList.add("displaynone");
+    document.querySelector(".stap2").classList.remove("displaynone");
+    document.querySelector("#stap3-sign").classList.remove("activestap");
+    document.querySelector("#stap2-sign").classList.add("activestap");
+};
+
+  return (
+    <>
+      <Head>
+          <title>AutoDraadloos.nl | Offerte</title>
+          <meta name="description" content=""/>
+          <meta name="og:description" content=""/>
+          <meta name="keywords" content=""/>
+          <meta property="og:url" content=""/>
+          <meta property="og:title" content=""/>
+      </Head>
+
+    <div className={styles.offerte}>
+        <div>
+
+
+
+
+
+            {/* Start offerte header */}
+            <div>
+                <ul>
+                    <li id="stap1-sign" className="activestap"><span>1</span> Kenteken</li>
+                    <li id="stap2-sign"><span>2</span> Wensen</li>
+                    <li id="stap3-sign"><span>3</span> Gegevens</li>
+                    <li id="stap4-sign"><span>4</span> Afronden</li>
+                </ul>
+            </div>
+            {/* End offerte header */}   
+
+            {/* Start offerte content */}
+            <div>
+
+                <section className="stap1">
+                    <h2>1. Voer uw kenteken in</h2>
+                    <p>Vul hieronder het kenteken van uw voertuig in om te beginnen met de offerte. Als u een buitenlands kenteken heeft neem dan contact met ons op.</p>
+
+                    <div className={`${styles.kenteken} kentekeninput`}>
+                        <div><Image src={euFlag} alt="Europese unie vlag"/><p>NL</p></div>
+                        <input type="text" id="kentekeninputfield" onChange={checkKenteken} maxLength={6}></input>
+                    </div>
+
+                    <div className={`${styles.kentekenloadingstate} kentekenloadingstate displaynone`}>
+                        <span></span>
+                        <p>Kenteken wordt gezocht...</p>
+                    </div>
+
+                    <button onClick={volgendeStap1} className={styles.volgendestap}>Volgende stap</button>
+                </section>
+
+
+                <section className="stap2 displaynone">
+                    <h2>2. Wat zijn uw wensen?</h2>
+                    <p>Hier komen de wensen</p>
+
+
+                    <button onClick={volgendeStap2} className={styles.volgendestap}>Volgende stap</button>
+                    <button onClick={vorigeStap1} className={styles.vorigestap}>Vorige stap</button>
+                </section>
+
+
+                <section className="stap3 displaynone">
+                    <h2>3. Wat zijn uw gegevens?</h2>
+                    <p>Hier komen de gegevens</p>
+
+                    <button onClick={vorigeStap2} className={styles.vorigestap}>Vorige stap</button>
+                </section>
+
+
+
+
+            </div>
+            {/* End offerte content */}
+
+
+
+
+
+        </div>
+    </div>
+    </>
+  );
+}
