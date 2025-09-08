63dcd76eae989ad9cba7ae0f6a18277bb92c2273
Muhammet <muhammet075@icloud.com>
2025-09-08 04:31:50 +0200

    adding favico

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,8 +10,7 @@ export default function App({ Component, pageProps }) {
         <meta name="format-detection" content="telephone=no"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
         <meta property="og:image" content="/logo.png" />
-        <link href="/favico-32.png" rel="icon" sizes="32x32" type="image/png"/>
-        <link href="/favico-16.png" rel="icon" sizes="16x16" type="image/png"/>
+        <link href="/favico.svg" rel="icon" type="image/svg"/>
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"/>
         <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
       </Head>
