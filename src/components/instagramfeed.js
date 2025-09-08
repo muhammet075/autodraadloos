d659dd17e6c12bc2b8dc008bba97d8316e862be9
Muhammet <muhammet075@icloud.com>
2025-09-08 04:13:57 +0200

    adding instagram widget

---
+++ src/components/instagramfeed.js
@@ -0,0 +1,22 @@
+import { useEffect, useState, useRef } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/instagramfeed.module.css";
+import instagramLogo from "@/assets/icons/instagram.svg";
+
+function Instagramfeed() {
+
+    useEffect(() => {
+    }, []);
+
+
+
+    return(      
+        <div className={styles.instagramfeed}>  
+            <span><h2>Volg ons op Instagram</h2><Link href="https://www.instagram.com/autodraadloos" target="_blank"><Image src={instagramLogo} alt="Instagram logo"/></Link></span>
+            <div class="elfsight-app-13e60a33-4a17-48d9-88c2-871c5823d4e6" data-elfsight-app-lazy></div>
+        </div>
+    )
+}
+  
+export default Instagramfeed;
