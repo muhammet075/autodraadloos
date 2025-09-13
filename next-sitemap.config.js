5e8389fa27ae22b6fb908162dc68ef9631ddeddb
Muhammet <muhammet075@icloud.com>
2025-09-13 05:18:43 +0200

    push sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://jouwdomein.nl', // vervang dit door je eigen domein
+  generateRobotsTxt: true, // maakt ook automatisch een robots.txt
+}
