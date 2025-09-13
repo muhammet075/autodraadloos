9366a8a850c3e216b4c3248b4d138a626a9a9c24
Muhammet <muhammet075@icloud.com>
2025-09-13 05:38:15 +0200

    clean up

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   siteUrl: 'https://www.autodraadloos.nl', // let op: geen trailing slash hier
   generateRobotsTxt: true,
-  sitemapSize: 50000, // verhoog dit zodat alles in één sitemap komt
+  sitemapSize: 50000,
 }
